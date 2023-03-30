75106106db429991240fc83dada2258618045470
Rodrigo Bossini <professorbossini@gmail.com>
2023-03-30 15:58:52 -0300

    apostila07(vetor-dinamico): secao 2.14 concluida

---
+++ TesteRedimensionamento.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+public class TesteRedimensionamento {
+  public static void main(String[] args)  throws Exception{
+    //70% adicionar um elemento
+    //30% remover um elemento
+    Random gerador = new Random();
+    VetorDinamico v = new VetorDinamico();
+    while (true){
+      double oQueFazer = gerador.nextDouble();
+      if(oQueFazer <= 0.7){
+
+      }
+      else{
+
+      }
+      Thread.sleep(5000);
+    }
+  }
+}
---
+++ VetorDinamico.java
@@ -0,0 +1,96 @@
+public class VetorDinamico{
+  private int [] elementos;
+  private int quantidade;
+  private int capacidade;
+  public VetorDinamico(){
+    //ajustar a capacidade do VetorDinamico
+    capacidade = 4;
+    elementos = new int[capacidade];
+  }
+
+   
+
+  //escrever um método que responde se um determinado elemento já existe na coleção
+  public boolean existe(int elemento){
+    for (int i = 0; i < quantidade; i++)
+      if (elementos[i] == elemento)
+        return true;
+    return false;
+  } 
+
+  public boolean estaCheio(){
+    // if (quantidade >= capacidade)
+    //   return true;
+    // return false;
+    //? :
+    // return quantidade >= capacidade ? true : false;
+    return quantidade >= capacidade;
+  }
+
+  public boolean estaVazio(){
+    return quantidade <= 0;
+  }
+
+  public void remover(){
+    if(!estaVazio()){
+      quantidade--;
+      if (quantidade <= capacidade / 4)
+        redimensionar(0.5);
+    }
+  }
+
+  public void adicionar(int elemento){
+    if(estaCheio())
+      redimensionar(2);
+    elementos[quantidade] = elemento;
+    quantidade++;
+    // if(!estaCheio()){
+    //   elementos[quantidade] = elemento;
+    //   quantidade++;
+    // }
+  }
+
+  private void aumentarCapacidade(){
+    //1. alocar um vetor "aux" com o dobro da capacidade
+    int [] aux = new int[capacidade * 2];
+    //2. fazer um for para copiar todos de elementos para aux
+    for (int i = 0; i < quantidade; i++){
+      aux[i] = elementos[i];
+    }
+    //3. fazer com que elementos referencie o objeto referenciado por aux
+    elementos = aux;
+    //4. dobrar o valor existente na variável capacidade
+    capacidade *= 2;
+  }
+  private void reduzirCapacidade(){
+    //1. alocar um vetor "aux" com a metade da capacidade
+    int [] aux = new int[capacidade / 2];
+    //2. fazer um for para copiar todos de elementos para aux
+    for (int i = 0; i < quantidade; i++){
+      aux[i] = elementos[i];
+    }
+    //3. fazer com que elementos referencie o objeto referenciado por aux
+    elementos = aux;
+    //4. dividir o valor existente na variável capacidade
+    capacidade /= 2;
+  }
+
+  private void redimensionar(double fatorRedimensionamento){
+    int [] aux = new int[(int)(capacidade * fatorRedimensionamento)];
+    for (int i = 0; i < quantidade; i++){
+      aux[i] = elementos[i];
+    }
+    elementos = aux;
+    capacidade = (int)(capacidade * fatorRedimensionamento);
+  }
+
+  
+
+  public void exibir(){
+    System.out.printf("Qtde: %d, Cap: %d.%n", quantidade, capacidade);
+    for (int i = 0; i < quantidade; i++){
+      System.out.print (elementos[i] + " ");
+    }
+    System.out.printf("%n");
+  }
+}
